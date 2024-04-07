731bc9e041f725a6a714ed3a26af49a24f8fb780
rivers07-maker <riosandres91@gmail.com>
2024-04-07 15:25:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 2000
+  },
+
+});
+
+
